f25fee1cfe06dbcd14877e6d79882614684730de
smadey <365649304@qq.com>
2014-03-11 16:31:51 +0800

    refine

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ exports.port = 18080;
 
 exports.cookieSecret = 'todo';
 
-exports.dbUrl = 'mongo://sa:sa@localhost:27017/todo'
+exports.dbUrl = 'mongo://sa:sa@localhost:27017/todo';
 
 exports.db = mongoskin.db(exports.dbUrl);
